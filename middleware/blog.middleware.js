1d22cc0ce2efa8c766f8ba35ab2795d62a7a304c
techrook <mondayitohowo3@gmail.com>
2022-11-06 08:44:02 +0100

    try

--- middleware/blog.middleware.js
+++ middleware/blog.middleware.js
@@ -11,7 +11,7 @@ async function confirmBlogAuthor(req, res, next){
     blogModel.findById(id)
     .then(blog =>{
         const blogAuthor = blog.author
-        if(author == blogAuthor){
+        if(author == authorId){
             next()
         }
     }).catch(err => {
