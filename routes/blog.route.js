a2985bf4fe3f9be8991690058eabed95126798a5
techrook <mondayitohowo3@gmail.com>
2022-12-13 01:34:39 +0100

    added input validation

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const passport = require('passport');
 
-
+//validators
+const blogValidator = require('../validator/blogValidator')
 const blogController = require('../controllers/blog.Controllers');
 const middleware = require('../middleware/blog.middleware')
 
@@ -9,7 +10,7 @@ const blogRouter = express.Router()
 
 blogRouter.get('/',blogController.countMiddleware,blogController.getAllBlogs );
 blogRouter.get('/:id',blogController.countMiddleware,blogController.getOneBlog );
-blogRouter.post('/:authorId',passport.authenticate('jwt', { session: false }), blogController.createBlog );
+blogRouter.post('/:authorId',passport.authenticate('jwt', { session: false }),blogValidator, blogController.createBlog );
 blogRouter.patch('/publishblog/:id', passport.authenticate('jwt', { session: false }),middleware.confirmBlogAuthor , blogController.publishBlog);
 blogRouter.patch('/:id',passport.authenticate('jwt', { session: false }), middleware.confirmBlogAuthor, blogController.updateBlog);
 blogRouter.delete('/:id',passport.authenticate('jwt', { session: false }), middleware.confirmBlogAuthor, blogController.deleteBlog);
---
+++ validator/blogValidator.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+
+const blogValidatorMiddleware = async (req, res , next) => {
+    const blogPayload = req.body
+    try {
+        await blogValidator.validateAsync(blogPayload);
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(406).send(error.details[0].message);
+    }
+}
+
+const blogValidator = Joi.object({
+    title: Joi.string()
+        .min(5)
+        .max(255)
+        .required(),
+    description: Joi.string()
+        .min(5)
+        .max(255)
+        .required(),
+    body: Joi.string()
+        .min(20)
+        .max(300)
+        .required(),
+    tags: Joi.string()
+})
+
+module.exports = blogValidatorMiddleware
