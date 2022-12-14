5f6ecb8e95ad8b428f015ecff4b8bc546ca4b0cb
techrook <mondayitohowo3@gmail.com>
2022-12-14 02:12:59 +0100

    added logs

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -7,10 +7,11 @@ const blogController = require('../controllers/blog.Controllers');
 const middleware = require('../middleware/blog.middleware')
 
 const blogRouter = express.Router()
+//blogvalidator malfunctioning 
 
 blogRouter.get('/',blogController.countMiddleware,blogController.getAllBlogs );
 blogRouter.get('/:id',blogController.countMiddleware,blogController.getOneBlog );
-blogRouter.post('/:authorId',passport.authenticate('jwt', { session: false }),blogValidator, blogController.createBlog );
+blogRouter.post('/:authorId',passport.authenticate('jwt', { session: false }),blogController.createBlog );
 blogRouter.patch('/publishblog/:id', passport.authenticate('jwt', { session: false }),middleware.confirmBlogAuthor , blogController.publishBlog);
 blogRouter.patch('/:id',passport.authenticate('jwt', { session: false }), middleware.confirmBlogAuthor, blogController.updateBlog);
 blogRouter.delete('/:id',passport.authenticate('jwt', { session: false }), middleware.confirmBlogAuthor, blogController.deleteBlog);
