ab464b1e5665a6c0b0d8b5b1093cccc1f32cf6dc
techrook <mondayitohowo3@gmail.com>
2023-02-28 00:53:48 +0100

    production fix

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ require('./DB/db').connect();
 
 const PORT = CONFIG.PORT || 3000;
 
-
+app.get('/', (req,res)=>{
+    logger.info('welcome to my blog')
+    return res.json({ status: true })
+    
+});
 
 app.listen(PORT, () => {
     console.log('Listening on port, ', PORT)
