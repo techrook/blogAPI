dac43ef321c45789d027b99ad72da5edf3426b7d
techrook <mondayitohowo3@gmail.com>
2023-03-01 00:02:21 +0100

    fixes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-
+// for test
 const app = require('./index')
 const CONFIG = require('./config/config')
 require('./DB/db').connect();
 
-const PORT = CONFIG.PORT || 3000;
+const PORT =  3000;
 
 
 
-app.get('/', (req,res)=>{
-    logger.info('welcome to my blog')
-    return res.json({ status: true })
+// app.get('/', (req,res)=>{
+//     logger.info('welcome to my blog')
+//     return res.json({ status: true })
     
-});
+// });
 
-app.listen(PORT, () => {
-    console.log('Listening on port, ', PORT)
-})
+// app.listen(PORT, () => {
+//     console.log('Listening on port, ', PORT)
+// })
 
